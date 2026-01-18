ad716c09c22c1303959d52fedb432d78368f36e7
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2026-01-18 12:50:19 +0100

    Setup a Spring Boot project with JUnit test support

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,50 @@
  * received a copy of the license along with this program.
  */
 
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
+
+plugins {
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.spring") version "2.2.21"
+    id("org.springframework.boot") version "4.0.1"
+}
+
+group = "com.github.tfkhim"
+version = "0.0.1-SNAPSHOT"
+description = "Recepies for JUnit test parallelization"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform(BOM_COORDINATES))
+
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("tools.jackson.module:jackson-module-kotlin")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
